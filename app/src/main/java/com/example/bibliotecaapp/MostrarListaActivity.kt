6d5050f07b602a9369b1ad0da12984b4f95848e7
Jose Carabantes <jose.carabantes@creativaconsultores.com>
2022-11-05 11:17:54 -0600

    Se agrego login/registro

--- app/src/main/java/com/example/bibliotecaapp/MostrarListaActivity.kt
+++ app/src/main/java/com/example/bibliotecaapp/MostrarListaActivity.kt
@@ -1,6 +1,5 @@
 package com.example.bibliotecaapp
 
-import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
@@ -8,16 +7,20 @@ import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bibliotecaapp.Adapters.PublicacionAdapter
+import com.example.bibliotecaapp.Interfaces.IOnClickListener
 import com.example.bibliotecaapp.MainActivity.Companion.publicacionRepository
+import com.example.bibliotecaapp.Models.Libro
+import com.example.bibliotecaapp.Models.Publicacion
 import com.example.bibliotecaapp.databinding.ActivityMostrarListaBinding
 
-class MostrarListaActivity : AppCompatActivity() {
+class MostrarListaActivity : AppCompatActivity(), IOnClickListener {
 
     // Variable para configurar viewBinding
     private lateinit var binding: ActivityMostrarListaBinding
     // Variables necesarias para configurar el recyclerview
     private lateinit var recyclerView: RecyclerView
     private lateinit var publicacionAdapter: PublicacionAdapter
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,24 +36,25 @@ class MostrarListaActivity : AppCompatActivity() {
             AlertDialog.Builder(this)
                 .setTitle(this.resources.getString(R.string.titulo_lista_vacia))
                 .setMessage(this.resources.getString(R.string.msg_lista_vacia))
-                .setPositiveButton(android.R.string.ok, DialogInterface.OnClickListener
-                { dialogInterface, i ->
+                .setPositiveButton(android.R.string.ok) { _, _ ->
                     finish()
-                }).show()
+                }.show()
         } else {
             // Configurar RecyclerView
             configRecyclerView()
         }
-        //configRecyclerView()
     }
 
     // Método que configura el recyclerview
     private fun configRecyclerView(){
         recyclerView = binding.rcPublicaciones
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this)
         publicacionAdapter = PublicacionAdapter(publicacionRepository.get(), this)
-        recyclerView.adapter = publicacionAdapter
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.layoutManager = linearLayoutManager
+            recyclerView.adapter = publicacionAdapter
+        }
     }
 
     // Método que configura el action bar
@@ -62,5 +66,20 @@ class MostrarListaActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun notifyItemChange(position: Int){
+        publicacionAdapter.notifyItemChanged(position)
+    }
+
+    override fun onClickListener(libro: Libro, position: Int) {
+        if(libro.Prestado()){
+            // Si el libro esta prestado, ejecutar devolucion
+            libro.devolver()
+        } else {
+            // El libro se encuentra disponible para ser prestado
+            libro.prestar()
+        }
+        notifyItemChange(position)
     }
 }
